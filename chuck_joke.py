2ef4b7cde2a965737b1d32dcd0470ebec694547f
Ярослав <Yagavdv14@yandex.ru>
2025-01-17 12:25:10 +0300

    Класс поместил в chuck_joke.py, инициализация экземпляра и вызов методов в main

---
+++ chuck_joke.py
@@ -0,0 +1,81 @@
+import requests
+
+
+class ChuckJoke:
+    base_url = 'https://api.chucknorris.io/jokes/random'
+    list_of_categories = None
+
+    def get_categories(self):
+        """
+        Получаем категории для шуток
+        """
+        try:
+            categories = requests.get('https://api.chucknorris.io/jokes/categories')
+            assert categories.status_code == 200, \
+                'Ошибка получения перечня категорий шуток.\nСписок категорий пуст.'
+        except Exception as err:
+            print(err.args[0])
+            self.list_of_categories = None
+        else:
+            categories.encoding = "utf-8"
+            self.list_of_categories = categories.json()
+
+    def get_random_joke(self):
+        """
+        Получение случайной шутки
+        """
+        print(f'Получение случайной шутки\n')
+        joke = requests.get(self.base_url)
+        assert joke.status_code == 200, \
+            'Ошибка получения случайной шутки'
+        joke.encoding = 'utf-8'
+        joke_text = joke.json().get("value")
+        assert 'Chuck' in joke_text, 'Полученная шутка не про Чака'
+        print(f'Шутка:\n\t{joke_text}\n')
+
+    def get_joke_from_selected_category(self, current_category):
+        """
+        Получение случайной шутки из заданной категории
+        :param current_category: категория случайной шутки
+        """
+        print(f'Получение случайной шутки из категории "{current_category}"\n')
+        joke_url = f'{self.base_url}?category={current_category}'
+        try:
+            joke = requests.get(joke_url)
+            assert joke.status_code == 200, \
+                'Ошибка статус-кода для случайной шутки из заданной категории'
+            assert joke.json().get("categories")[0] == current_category, \
+                f'Категория шутки: "{joke.json().get("categories")[0]}", ожидалось: "{current_category}"'
+            joke.encoding = 'utf-8'
+            joke_text = joke.json().get("value")
+            assert 'Chuck' in joke_text, \
+                'Полученная шутка не про Чака'
+        except Exception as err:
+            print(err.args[0])
+        else:
+            print(f'Шутка:\n\t{joke_text}\n')
+
+    def get_one_joke_from_each_category(self):
+        """
+        Получение по одной случайно шутке из каждой категории
+        """
+        self.get_categories()
+        if self.list_of_categories:
+            for current_category in self.list_of_categories:
+                self.get_joke_from_selected_category(current_category)
+
+
+    def get_one_joke_from_user_category(self):
+        """
+        Получение случайной шутки из категории введенной пользователем
+        """
+        self.get_categories()
+        if self.list_of_categories:
+            print('Доступны следующие категории шуток:\n')
+            for category in self.list_of_categories:
+                print(category, end=' ')
+            current_category = input('\n\nВведите наименование интересующей категории:\n')
+            if current_category in self.list_of_categories:
+                self.get_joke_from_selected_category(current_category)
+            else:
+                print('Указанной категории не существует.')
--- main.py
+++ main.py
@@ -1,50 +1,11 @@
-import requests
-from random import randint
-
-
-class ChuckJoke:
-    base_url = 'https://api.chucknorris.io/jokes/random'
-    list_of_categories = None
-
-    def __init__(self):
-        """
-        При инициализации экземпляра класса получаем категории для шуток
-        """
-        try:
-            categories = requests.get('https://api.chucknorris.io/jokes/categories')
-            assert categories.status_code == 200, \
-                'Ошибка получения перечня категорий шуток'
-        except Exception as err:
-            print(err.args[0])
-        else:
-            categories.encoding = "utf-8"
-            self.list_of_categories = categories.json()
-
-    def get_joke(self):
-        """
-        Получение шутки из случайной категории, при ошибке загрузки категорий будет получена случайная шутка
-        """
-        if self.list_of_categories:
-            num_category = randint(0, len(self.list_of_categories) - 1)
-            current_category = self.list_of_categories[num_category]
-            print(f'Получение случайной шутки из категории "{current_category}"\n')
-            joke_url = f'{self.base_url}?category={current_category}'
-            joke = requests.get(joke_url)
-            assert joke.status_code == 200, \
-                'Ошибка получения случайной шутки из заданной категории'
-            assert joke.json().get("categories")[0] == current_category, \
-                f'Получена категория шутки: "{joke.json().get("categories")[0]}", ожидалось: "{current_category}"'
-        else:
-            print(f'Получение случайной шутки из случайной категории\n')
-            joke = requests.get(self.base_url)
-            assert joke.status_code == 200, \
-                'Ошибка получения случайной шутки'
-
-        joke.encoding = 'utf-8'
-        joke_text = joke.json().get("value")
-        assert 'Chuck' in joke_text, 'Полученная шутка не про Чака'
-        print(f'Шутка:\n\t{joke_text}')
-
+from chuck_joke import ChuckJoke
 
 chuck_joke = ChuckJoke()
-chuck_joke.get_joke()
+
+# случайная шутка из каждой категории
+chuck_joke.get_one_joke_from_each_category()
+
+# Следующее ДЗ
+# chuck_joke.get_one_joke_from_user_category()
+
+
